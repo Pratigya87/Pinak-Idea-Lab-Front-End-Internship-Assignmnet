cdb2b52cf33ec72f40dab4b2dc234e9f6157df19
Pratigya Patidar <141490067+Pratigya87@users.noreply.github.com>
2024-10-17 21:45:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".service-box").forEach((box) => {
+    box.addEventListener("mouseenter", () => {
+      anime({
+        targets: box,
+        rotate: "1turn",
+        duration: 1000,
+        easing: "easeInOutQuad",
+      });
+    });
+  });
+});
